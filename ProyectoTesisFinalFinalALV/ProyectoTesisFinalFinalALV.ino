a7689891d653e4ce7f45f1a9e914f7d7d933feb3
guillenortizrodrigo <79247023+guillenortizrodrigo@users.noreply.github.com>
2022-06-25 14:54:24 -0500

    rorro updates

--- ProyectoTesisFinalFinalALV/ProyectoTesisFinalFinalALV.ino
+++ ProyectoTesisFinalFinalALV/ProyectoTesisFinalFinalALV.ino
@@ -1,13 +1,16 @@
 #include <SPI.h>
 
 int slaveSelect = 2;
-int delaytime = 200;
-int i = 0; 
 
-void transferData(int stepNumber, byte byte1, byte byte2){
-  
+//ARREGLOS DE DATOS
+const int slavesquantity = 1;
+int slaves[slavesquantity] = {2};
+
+int delaytime = 200;
+
+void transferData(int stepNumber, byte byte1, byte byte2,int slave){
   byte response; 
-  
+  digitalWrite(slave,LOW);
   response = SPI.transfer(stepNumber); 
   Serial.println(response, BIN); 
   
@@ -15,8 +18,8 @@ void transferData(int stepNumber, byte byte1, byte byte2){
   Serial.println(response, BIN); 
   
   response = SPI.transfer(byte2); 
-  Serial.println(response, BIN); 
-
+  Serial.println(response, BIN);
+  digitalWrite(slave,HIGH);
 }
 
 void setup() {
@@ -26,25 +29,23 @@ void setup() {
     SPI.setClockDivider(SPI_CLOCK_DIV128);
     SPI.setBitOrder(MSBFIRST);
     SPI.setDataMode(SPI_MODE0);
+
     
-    for(int stepNumber=1; stepNumber<5;stepNumber++){
-      transferData(stepNumber, B11110000, B10101010);
+    for(int i=0; i<slavesquantity; i++){
+      for(int stepNumber=1; stepNumber<5;stepNumber++){
+        transferData(stepNumber, B11110000, B10101010,slaves[i]);
+      }
     }
+    
+
+    digitalWrite(slaveSelect,LOW);
+    byte response; 
+    response = SPI.transfer(25); 
+    Serial.println(response, BIN); 
+    digitalWrite(slaveSelect,HIGH);
     
  }
  
 void loop() {
-    if (i >= 256){
-      //reiniciamos el contador
-      i = 0; 
-    }
     
-    digitalWrite(slaveSelect,LOW);
-    int m = SPI.transfer(i);
-    digitalWrite(slaveSelect,HIGH);
-    delay(delaytime);
-    Serial.println(m);
-    
-    
-    i++;
 }
