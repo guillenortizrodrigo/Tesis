d0ec887cf2e2ccf9dcffd3e8803bc5d3ca83180a
Pablo Huerta <45541378+contreraspablo9@users.noreply.github.com>
2022-08-23 20:32:49 -0500

    volts2bits test success!

--- ProyectoTesisFinalFinalALV/ProyectoTesisFinalFinalALV.ino
+++ ProyectoTesisFinalFinalALV/ProyectoTesisFinalFinalALV.ino
@@ -1,16 +1,44 @@
 #include <SPI.h>
+const int clock = 7; 
+
 
 int slaveSelect = 2;
 
 //ARREGLOS DE DATOS
 const int slavesquantity = 1;
-int slaves[slavesquantity] = {2};
+int slaves[slavesquantity] = {
+  2
+};
+
+float voltageMaxSet[slavesquantity] = {
+  1.5 //volts
+}; 
+float voltageMinSet[slavesquantity] = {
+  1
+}; 
+float delta[slavesquantity] = {
+  .1
+}; 
+float measurementVoltage[slavesquantity] = {
+  .5
+}; 
+float voltageMaxReset[slavesquantity] = {
+  -22141
+}; 
+float voltageMinReset[slavesquantity] = {
+  -10485
+}; 
+float targetCurrent[slavesquantity] = {
+  .000030
+}; 
+
+
 
 int delaytime = 200;
 
-void transferData(int stepNumber, byte byte1, byte byte2,int slave){
+void transferData(int stepNumber, byte byte1, byte byte2,int slavePin){
   byte response; 
-  digitalWrite(slave,LOW);
+  digitalWrite(slavePin,LOW);
   response = SPI.transfer(stepNumber); 
   Serial.println(response, BIN); 
   
@@ -19,7 +47,16 @@ void transferData(int stepNumber, byte byte1, byte byte2,int slave){
   
   response = SPI.transfer(byte2); 
   Serial.println(response, BIN);
-  digitalWrite(slave,HIGH);
+  digitalWrite(slavePin,HIGH);
+}
+
+int volts2bits(float volts){
+  // convertir el voltaje ingresado por el usuario a 
+  // bits para ser manejado por el programa. 
+  float bits; 
+  bits = volts * 13107; 
+  
+  return int(bits); 
 }
 
 void setup() {
@@ -29,11 +66,29 @@ void setup() {
     SPI.setClockDivider(SPI_CLOCK_DIV128);
     SPI.setBitOrder(MSBFIRST);
     SPI.setDataMode(SPI_MODE0);
+    
+    pinMode(clock, OUTPUT); //manejo de la interrupcion 
 
     
     for(int i=0; i<slavesquantity; i++){
       for(int stepNumber=1; stepNumber<5;stepNumber++){
-        transferData(stepNumber, B11110000, B10101010,slaves[i]);
+        switch (stepNumber){
+          case 1: 
+            transferData(stepNumber, ( uint8_t )( volts2bits(voltageMaxSet[i]) ),( uint8_t )( volts2bits(voltageMaxSet[i])>>8 ),slaves[i]);
+            break; 
+          case 2: 
+            transferData(stepNumber, ( uint8_t )( volts2bits(voltageMaxSet[i]) ), ( uint8_t )( volts2bits(voltageMaxSet[i])>>8 ),slaves[i]);
+            break; 
+          case 3: 
+            transferData(stepNumber, ( uint8_t )( volts2bits(voltageMaxSet[i]) ), ( uint8_t )( volts2bits(voltageMaxSet[i])>>8 ),slaves[i]);
+            break; 
+          case 4: 
+            transferData(stepNumber, ( uint8_t )( volts2bits(measurementVoltage[i]) ), ( uint8_t )( volts2bits(measurementVoltage[i])>>8 ),slaves[i]);
+            break; 
+          default: {}
+          
+        
+        }
       }
     }
     
@@ -47,5 +102,9 @@ void setup() {
  }
  
 void loop() {
-    
+  
+    digitalWrite (clock , HIGH); 
+  delayMicroseconds(50);
+  digitalWrite(clock, LOW); 
+  delayMicroseconds(50);
 }
